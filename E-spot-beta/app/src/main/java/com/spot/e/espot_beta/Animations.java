430560d388386282d7e481e681df8c1cc64482ea
joseph muli <jayjaymuli00@gmail.com>
2016-03-08 22:46:38 +0300

    animation

--- E-spot-beta/app/src/main/java/com/spot/e/espot_beta/Animations.java
+++ E-spot-beta/app/src/main/java/com/spot/e/espot_beta/Animations.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by imaya on 3/8/16.
@@ -13,40 +18,43 @@ import android.widget.Toast;
 public class Animations extends ListActivity {
 
 
-    String [] animations =
-            {
-                    "Home",
-                    "Zootopia",
-                    "Angry Birds",
-                    "KungFu panda 3",
-                    "Finding Dory",
-                    "Ratchet and Clank Focus",
-                    "Ice Age: Collision Course",
-                    "The Secret Life of Pets",
-                    "Norm of the North",
-                    "Sing",
-                    "Minions",
-                    "The Good Dinosaur",
-                    "Trolls",
-                    "Moana",
-                    "The Red Turtle",
-                    "The Monk and the Fish",
-                    "Cinderella ",
-                    "Sly Cooper",
-
-            };
+//    String [] animations =
+//            {
+//                    "Home",
+//                    "Zootopia",
+//                    "Angry Birds",
+//                    "KungFu panda 3",
+//                    "Finding Dory",
+//                    "Ratchet and Clank Focus",
+//                    "Ice Age: Collision Course",
+//                    "The Secret Life of Pets",
+//                    "Norm of the North",
+//                    "Sing",
+//                    "Minions",
+//                    "The Good Dinosaur",
+//                    "Trolls",
+//                    "Moana",
+//                    "The Red Turtle",
+//                    "The Monk and the Fish",
+//                    "Cinderella ",
+//                    "Sly Cooper",
+//
+//            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.movies);
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, animations));
+        //setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, movies));
+        String[] animations = getResources().getStringArray(R.array.animations);
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.animations, R.id.label, animations));
+
     }
 
-    public void onListItemClick(
-            ListView parent, View v, int position, long id)
-    {
-        Toast.makeText(this, "Your Choice is " + animations[position], Toast.LENGTH_LONG).show();
-    }
+//    public void onListItemClick(
+//            ListView parent, View v, int position, long id)
+//    {
+//        Toast.makeText(this, "Your Choice is " +animations[position], Toast.LENGTH_LONG).show();
+//    }
 }
 
